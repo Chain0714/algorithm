384336291361c1d540defa3598b3a595f221bed4
Chain0714 <657775391@qq.com>
2020-06-17 17:18:41 +0800

    297. 二叉树的序列化与反序列化 official

---
+++ src/main/java/org/chain/daily/no297/SolutionOfficial.java
@@ -0,0 +1,43 @@
+package org.chain.daily.no297;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class SolutionOfficial {
+    public String rserialize(TreeNode root, String str) {
+        if (root == null) {
+            str += "None,";
+        } else {
+            str += root.val + ",";
+            str = rserialize(root.left, str);
+            str = rserialize(root.right, str);
+        }
+        return str;
+    }
+
+    public String serialize(TreeNode root) {
+        return rserialize(root, "");
+    }
+
+    public TreeNode rdeserialize(List<String> l) {
+        if (l.get(0).equals("None")) {
+            l.remove(0);
+            return null;
+        }
+
+        TreeNode root = new TreeNode(Integer.valueOf(l.get(0)));
+        l.remove(0);
+        root.left = rdeserialize(l);
+        root.right = rdeserialize(l);
+
+        return root;
+    }
+
+    public TreeNode deserialize(String data) {
+        String[] data_array = data.split(",");
+        List<String> data_list = new LinkedList<String>(Arrays.asList(data_array));
+        return rdeserialize(data_list);
+    }
+
+}
