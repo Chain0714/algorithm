2bf3ec65500faf910e0f9fba9402249a70b73ade
Chain0714 <657775391@qq.com>
2020-06-28 21:36:23 +0800

    139. 单词拆分（动态规划）

---
+++ src/main/java/org/chain/daily/no139/Solution.java
@@ -0,0 +1,67 @@
+package org.chain.daily.no139;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 139. 单词拆分
+ * 给定一个非空字符串 s 和一个包含非空单词列表的字典 wordDict，判定 s 是否可以被空格拆分为一个或多个在字典中出现的单词。
+ * <p>
+ * 说明：
+ * <p>
+ * 拆分时可以重复使用字典中的单词。
+ * 你可以假设字典中没有重复的单词。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入: s = "leetcode", wordDict = ["leet", "code"]
+ * 输出: true
+ * 解释: 返回 true 因为 "leetcode" 可以被拆分成 "leet code"。
+ * <p>
+ * 示例 2：
+ * <p>
+ * 输入: s = "applepenapple", wordDict = ["apple", "pen"]
+ * 输出: true
+ * 解释: 返回 true 因为 "applepenapple" 可以被拆分成 "apple pen apple"。
+ * 注意你可以重复使用字典中的单词。
+ * <p>
+ * 示例 3：
+ * <p>
+ * 输入: s = "catsandog", wordDict = ["cats", "dog", "sand", "and", "cat"]
+ * 输出: false
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/word-break
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Solution {
+    /**
+     * 动态规划  f（i） == f（j） && check（s[j,i-1])
+     */
+    public boolean wordBreak(String s, List<String> wordDict) {
+        Set<String> wordDictSet = new HashSet<>(wordDict);
+        boolean[] dp = new boolean[s.length() + 1];
+        //j==0,只需要判断[1-s.length]
+        dp[0] = true;
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (dp[j] && wordDictSet.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp[s.length()];
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        List<String> wordDict = new ArrayList<String>(){{
+            add("leet");
+            add("code");
+        }};
+        System.out.println(solution.wordBreak("leetcode",wordDict));
+    }
+}
