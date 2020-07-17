0047f110fb52640373233e97ac72fc3a6a8fe5f8
Chain0714 <657775391@qq.com>
2020-07-17 15:13:07 +0800

    面试题 17.13. 恢复空格 309. 最佳买卖股票时机含冷冻期

---
+++ src/main/java/org/chain/daily/ms1713/Trie.java
@@ -0,0 +1,27 @@
+package org.chain.daily.ms1713;
+
+/**
+ * 字典树，前缀树
+ */
+public class Trie {
+    public Trie[] next;
+    public boolean isEnd;
+
+    public Trie() {
+        next = new Trie[26];
+        isEnd = false;
+    }
+
+    public void insert(String s) {
+        Trie curPos = this;
+
+        for (int i = s.length() - 1; i >= 0; --i) {
+            int t = s.charAt(i) - 'a';
+            if (curPos.next[t] == null) {
+                curPos.next[t] = new Trie();
+            }
+            curPos = curPos.next[t];
+        }
+        curPos.isEnd = true;
+    }
+}
